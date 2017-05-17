4fcaed156d664cb2979de28ce22619aa1dbdffa1
Karen Santos <karen.santos@mecomo.com.br>
2017-05-17 15:01:18 -0300

    Gradle fixes

--- test/RoutinLogicTest.java
+++ test/RoutinLogicTest.java
@@ -1,3 +1,22 @@
+import org.junit.Test;
 
 public class RoutinLogicTest {
+
+//    Test Case #1 - Standard
+//
+//    Input
+//
+//    Brazil Keyboard 2
+//    France Mouse 2
+//
+//    DHL, None
+//    Keyboard 2
+//    Output
+//
+//    Brazil Keyboard 2
+    @Test
+    public void ifReceivesAnEmptyStringReturns0(){
+        assertThat(0, equalTo(0));
+    }
+
 }
